e51adff23e2c0e59174be99b780b68bad654bdd2
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-22 19:15:32 -0300

    feat: implement payment entity

---
+++ Entities/Payment.cs
@@ -0,0 +1,15 @@
+﻿using E_commerce_API.Enums;
+
+namespace E_commerce_API.Entities
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public PaymentMethod Method { get; set; } = null!;
+        public PaymentStatus Status { get; set; } = null!;
+    }
+}
