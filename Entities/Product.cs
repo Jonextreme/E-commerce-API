0a1ccb5db7f4c26998963ec3717a470ad5b8907b
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-22 19:15:49 -0300

    feat: implement product entity

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -2,5 +2,13 @@
 {
     public class Product
     {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
 }
