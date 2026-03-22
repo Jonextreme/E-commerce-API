f04279135aaff6f722e949d50c2b339c1304190e
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-22 19:13:46 -0300

    feat: implement orderItem entity

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace E_commerce_API.Entities
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
