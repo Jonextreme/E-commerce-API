e71a8b07b76b242e9244b931d1979ff67325cde4
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-22 19:13:27 -0300

    feat: implement order entity

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -1,6 +1,17 @@
-﻿namespace E_commerce_API.Entities
+﻿using E_commerce_API.Enums;
+
+namespace E_commerce_API.Entities
 {
     public class Order
     {
+        public int OrderId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public OrderStatus Status { get; set; } = null!;
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+        public Payment Payment { get; set; } = null!;
+        public Shipping Shipping { get; set; } = null!;
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
 }
