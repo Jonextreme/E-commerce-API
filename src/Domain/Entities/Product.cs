871594545176dde568fbab301deff6ddb6f78d0a
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-24 16:24:28 -0300

    refactor: add cartItems navigation property to product

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -9,6 +9,7 @@
         public int Stock { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; } = null!;
+        public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
         public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
 }
