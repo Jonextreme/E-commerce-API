fc561c6e8acfdda78489725d1e616c5f2352fcf5
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-04-01 14:44:18 -0300

    feat: apply Fluent API configurations in AppDbContext

--- src/Infrastructure/Data/AppDbContext.cs
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -1,12 +1,31 @@
 ﻿using E_commerce_API.src.Domain.Entities;
+using E_commerce_API.src.Infrastructure.Data.Configuration;
 using Microsoft.EntityFrameworkCore;
 
-namespace E_commerce_API.Context
+namespace E_commerce_API.src.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CartConfiguration());
+            modelBuilder.ApplyConfiguration(new CartItemConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+            modelBuilder.ApplyConfiguration(new PaymentConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new ShippingConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
