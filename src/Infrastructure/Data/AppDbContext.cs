cac96b47115a2dbcc997b9c8a21ca2020ea7e658
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-04-07 21:57:42 -0300

    fix: add missings DbSets in AppDbContext

--- src/Infrastructure/Data/AppDbContext.cs
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -6,9 +6,15 @@ namespace E_commerce_API.src.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Shipping> Shippings { get; set; }
+        public DbSet<User> Users { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
