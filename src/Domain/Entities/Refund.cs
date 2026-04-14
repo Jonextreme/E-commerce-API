585254800856fb164d8ad75e21c0bec1f1ef8629
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-04-14 14:17:53 -0300

    feat: add refund entity

---
+++ src/Domain/Entities/Refund.cs
@@ -0,0 +1,12 @@
+﻿namespace E_commerce_API.src.Domain.Entities
+{
+    public class Refund
+    {
+        public int RefundId { get; set; }
+        public int OrderItemId { get; set; }
+        public OrderItem OrderItem { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public int Quantity { get; set; }
+        public DateTime RefundDate { get; set; }
+    }
+}
